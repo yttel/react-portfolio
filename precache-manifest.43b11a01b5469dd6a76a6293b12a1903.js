f4cc3287fbae77e0cf132d2584eef5b148ba20e0
BytteL <lettybedard@gmail.com>
2020-06-07 22:52:02 -0400

    Updates

---
+++ precache-manifest.43b11a01b5469dd6a76a6293b12a1903.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42ca8bf58a768c514b8e9352d056d08",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "12da98c39996aa8f807e",
+    "url": "/react-portfolio/static/js/2.c271f939.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.c271f939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495bde43c33e0deebece",
+    "url": "/react-portfolio/static/js/main.31464f8c.chunk.js"
+  },
+  {
+    "revision": "37db3f26b7c0ea36aed0",
+    "url": "/react-portfolio/static/js/runtime-main.d08fb9a1.js"
+  }
+]);
