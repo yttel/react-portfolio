57ca1ef0f536330354c33f85ac84c85ee02587e7
BytteL <lettybedard@gmail.com>
2020-06-07 21:03:27 -0400

    portfolio cards and links done, needs img and text

--- client/src/components/ProjectCard/index.js
+++ client/src/components/ProjectCard/index.js
@@ -1,8 +1,24 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 
-const ProjectCard = () => {
+const ProjectCard = ({ name, description, screenshot, githubUrl, deployedUrl }) => {
   return (
-    <h3>PROJECT CARD</h3>
+    <>
+      {/* <h3>PROJECT CARD</h3> */}
+      <Card className="text-center mb-3">
+        <Card.Header>
+          <Card.Title>{name}</Card.Title>
+        </Card.Header>
+        <Card.Img variant="top" src={screenshot} />
+        <Card.Body>
+          <Card.Text>{description}</Card.Text>
+          <a class="btn mr-2 border"  href={githubUrl} target="_blank" rel="noopener noreferrer">Source Code</a>
+          {
+            deployedUrl && <a class="btn border" href={deployedUrl} target="_blank" rel="noopener noreferrer">Deployed</a>
+          }
+        </Card.Body>
+      </Card>
+    </>
   );
 };
 
--- client/src/pages/Portfolio/index.js
+++ client/src/pages/Portfolio/index.js
@@ -1,8 +1,24 @@
 import React from "react";
+import projects from "../../data/works.json"
+import ProjectCard from "../../components/ProjectCard"; 
+import { Container, Row, Col } from "react-bootstrap";
+
+const projectList = projects.map((project) => {
+  return (
+    <Col>
+      <ProjectCard {...project} />
+    </Col>
+  );
+});
 
 const Portfolio = () => {
   return (
-    <h1>PORTFOLIO</h1>
+    <Container fluid>
+      <h1 class="text-center">Portfolio</h1>
+      <Row sm={1} md={2} lg={3}>
+          { projectList }
+      </Row>
+    </Container>
   );
 };
 
